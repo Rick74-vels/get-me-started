8757da29abd90a1fc3a807eb205482bcdde72cfc
Ando <devtac.avelino.ando@gmail.com>
2019-11-26 10:44:18 -0800

    first commint

---
+++ select.php
@@ -0,0 +1,28 @@
+<!-- show the stored data inside the database -->
+<ul>
+<?php
+$db = new mysqli(
+    'localhost',
+    'root',
+    'password',
+    'phpt'
+);
+$sql = 'SELECT * FROM tip';
+$result = $db->query($sql);
+
+foreach ($result as $row) {
+    printf(
+        '<li><span style="color: %s">%s (%s)</span>
+        <a href="update.php?id=$s">update</a>
+        <a href="delete.php?id=%s">delete</a>   
+        </li>',
+        htmlspecialchars($row['color'], ENT_QUOTES),
+        htmlspecialchars($row['name'], ENT_QUOTES),
+        htmlspecialchars($row['gender'], ENT_QUOTES),
+        htmlspecialchars($row['id'], ENT_QUOTES),
+        htmlspecialchars($row['id'], ENT_QUOTES)
+    );
+    $db->close();
+}
+?>
+</ul>
