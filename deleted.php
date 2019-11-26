4909a2c34879e649285443f88e40936013d0b101
Ando <devtac.avelino.ando@gmail.com>
2019-11-26 14:47:49 -0800

    add new vendor

--- deleted.php
+++ deleted.php
@@ -1,4 +1,6 @@
+<!-- this coment only added to test if the file is connected to gitHub -->
 <?php
+//new text added on purpose
 //delete.php?id=2
 if (isset($_GET['id']) && ctype_digit($_GET['id'])) {
     $id = $_GET['id'];
